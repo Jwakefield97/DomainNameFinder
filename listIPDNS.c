7218487130bc8256c279749f4a838b41c37eb44b
jake <jacobstevenwakefield@gmail.com>
2018-11-11 03:06:46 -0600

    add basic ip increment

--- listIPDNS.c
+++ listIPDNS.c
@@ -18,14 +18,26 @@
 int main(int argc, char **argv){
     char *subnetMask = argv[1];
     char *dottedIp = argv[2]; 
+    char *maxIp = "255.255.255.255";
 
     unsigned long int ip = inet_addr(dottedIp);
     unsigned long int subnet = inet_addr(subnetMask);   
-    unsigned long int calc = ip & subnet; //get the network portion of the address
-    struct in_addr ans = {calc}; //the dotted decimal of the netowrk portion of the address
+    unsigned long int max = inet_addr(maxIp);   
+    unsigned long int numHosts = max/(subnet & max);   
+
+    unsigned long int network = ip & subnet; //get the network portion of the address
+    struct in_addr ans = {network}; //the dotted decimal of the netowrk portion of the address
 
     printf("getting dns for %s %s\n",subnetMask,dottedIp);
-    printf("subnet: %lu\nip: %lu\nnet: %lu\nnet str: %s\nnum hosts: %lu\n",subnet,ip,calc,inet_ntoa(ans),subnet-calc);
+    printf("subnet: %lu\nip: %lu\nnet: %lu\nnet str: %s\nnum hosts: %lu\n",subnet,ip,network,inet_ntoa(ans),numHosts);
+    for (int i=0; i<numHosts; i++) {
+        unsigned long int addr = network;
+        addr = ntohl(addr);
+        addr = addr+i;
+        addr = htonl(addr);
+        struct in_addr x = {addr};
+        printf("%d:\t%s\n",i,inet_ntoa(x));
+    }
     
     //TODO: get all of the valid ips from the given subnet
     //TODO: loop through valid ip's and get their dns info 
