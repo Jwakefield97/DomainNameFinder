52c1c3201219739b2e7499eb4f795207c9085c63
jake <jacobstevenwakefield@gmail.com>
2018-11-11 02:10:16 -0600

    stash changes

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ clean:
 fresh: clean all
 
 run: 
-	./listIPDNS 255.255.255 192.168.1
+	./listIPDNS 255.255.255.0 192.168.1.0
 
 pack: clean
 	mkdir jsw5227
--- listIPDNS.c
+++ listIPDNS.c
@@ -8,13 +8,25 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
 
 int main(int argc, char **argv){
     char *subnetMask = argv[1];
     char *dottedIp = argv[2]; 
-    printf("getting dns for %s %s",subnetMask,dottedIp);
+
+    unsigned long int ip = inet_addr(dottedIp);
+    unsigned long int subnet = inet_addr(subnetMask);   
+    unsigned long int calc = ip & subnet; //get the network portion of the address
+    struct in_addr ans = {calc}; //the dotted decimal of the netowrk portion of the address
+
+    printf("getting dns for %s %s\n",subnetMask,dottedIp);
+    printf("subnet: %lu\nip: %lu\nnet: %lu\nnet str: %s\nnum hosts: %lu\n",subnet,ip,calc,inet_ntoa(ans),subnet-calc);
+    
     //TODO: get all of the valid ips from the given subnet
     //TODO: loop through valid ip's and get their dns info 
     //TODO: print out the ip and the host name for each ip.
