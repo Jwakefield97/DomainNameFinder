873d00a1855b52440b2c3a7adec9132369801adc
jake <jacobstevenwakefield@gmail.com>
2018-11-11 00:27:32 -0600

    add comments

--- listIPDNS.c
+++ listIPDNS.c
@@ -1,3 +1,11 @@
+//********************************************************************
+// Jacob Wakefield
+// Course: Computer Networks
+// Programming Project #1: List IP DNS
+// Due date: November 17, 2018 
+// Instructor: Dr. Ajay K. Katangur
+//********************************************************************
+
 #include <stdio.h>
 #include <string.h>
 
@@ -7,4 +15,7 @@ int main(int argc, char **argv){
     char *subnetMask = argv[1];
     char *dottedIp = argv[2]; 
     printf("getting dns for %s %s",subnetMask,dottedIp);
+    //TODO: get all of the valid ips from the given subnet
+    //TODO: loop through valid ip's and get their dns info 
+    //TODO: print out the ip and the host name for each ip.
 }
